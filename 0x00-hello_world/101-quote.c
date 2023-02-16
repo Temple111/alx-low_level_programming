47c246958ada12c2eba2b168a89c80f39e134f14
Temple111 <eseigbetemple122@gmail.com>
2023-02-16 05:32:57 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-gcc -S -masn=intel $CFILE
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - a c program that prints a line to the standard error
+ * Return 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", standard output);
+return (1);
+}
