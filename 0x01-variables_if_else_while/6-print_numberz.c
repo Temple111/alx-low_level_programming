45a6b2e88bc390bff130adf70899eaa21103b458
Temple111 <eseigbetemple122@gmail.com>
2023-02-17 11:36:50 -0800

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this program checks if the last digit of anumber is >, < or zero
+ * Return: 0 (sucessful)
+ */
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
