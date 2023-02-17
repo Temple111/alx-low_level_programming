0896a2dc849802ec1428168ea22e80392edf6fbf
Temple111 <eseigbetemple122@gmail.com>
2023-02-17 11:24:05 -0800

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - this program checks if the last digit of anumber is >, < or zero
+ * Return: 0 (sucessful)
+ */
+int main(void)
+{
+	char b;
+
+	b = 'a';
+	while
+		(b <= 'z') {
+			if ((b != 'q' && b != 'e') && b <= 'z')
+				putchar(b);
+			b++;
+		}
+	putchar('\n');
+	return (0);
+}
