82954e52f0fe308b1d139bcc995c51d7dd6ed6ab
Temple111 <eseigbetemple122@gmail.com>
2023-02-17 10:45:04 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this program checks if the last digit of anumber is >, < or zero
+ * Return: 0 (sucessful)
+ */
+int main(void)
+{
+char c;
+
+c = 'a';
+while
+	(c <= 'z') {
+		putchar(c);
+		c++;
+	}
+putchar('\n');
+return (0);
+}
