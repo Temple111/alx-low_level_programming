0ddcf2d1cfc4b91db25df4746ead7a4a869b7347
Temple111 <eseigbetemple122@gmail.com>
2023-02-17 11:30:02 -0800

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this program checks if the last digit of anumber is >, < or zero
+ * Return: 0 (sucessful)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
