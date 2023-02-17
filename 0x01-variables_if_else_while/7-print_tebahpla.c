7f21d37a5dd9d0c926c7dc5e0d2ad230894ec403
Temple111 <eseigbetemple122@gmail.com>
2023-02-17 11:44:44 -0800

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - this program checks if the last digit of anumber is >, < or zero
+ * Return: 0 (sucessful)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
