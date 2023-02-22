fcd0d26400cb041bced49048e36f5e0208916447
Temple111 <eseigbetemple122@gmail.com>
2023-02-21 17:26:57 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,11 +2,11 @@
 
 /**
  * main - prints the first 98 fibonacci numbers, starting with
- * 1 and 2, separated by a comma followed by a space
+ *1 and 2, separated by a comma followed by a space
  * Return: 0 (good)
  */
 
-int Main(void)
+int main(void)
 
 {
 	int count;
