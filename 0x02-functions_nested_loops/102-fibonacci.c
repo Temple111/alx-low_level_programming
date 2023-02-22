90d25d8695bd8f819093afecfdd67beb6e06dce3
Temple111 <eseigbetemple122@gmail.com>
2023-02-21 17:32:50 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,17 +13,17 @@ int main(void)
 
 	while (i < 50)
 	{
-		if (i == 0)
-			printf("%d", j);
-		else if (i == 1)
-			printf(", %ld", k);
-		else
-		{
-			k += j;
-			j = k - j;
-			printf(", %ld", k);
-		}
-		++i;
+	if (i == 0)
+	printf("%ld", j);
+	else if (i == 1)
+	printf(", %ld", k);
+	else
+	{
+	k += j;
+	j = k - j;
+	printf(", %ld", k);
+	}
+	++i;
 	}
 	printf("\n");
 	return (0);
