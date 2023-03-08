565da61320886515204d51a132ca876c18cefbcc
Temple111 <eseigbetemple122@gmail.com>
2023-03-08 03:49:58 -0800

    4-pow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - calculate the root of a number.
+ * @x: the numbers root to be found
+ * @y: the number to raised to
+ * Return: always success
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
