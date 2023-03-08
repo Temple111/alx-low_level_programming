c11010a5f8a046f2af07dd394ab82cc2c522c43d
Temple111 <eseigbetemple122@gmail.com>
2023-03-08 06:02:42 -0800

    100-palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -19,6 +19,10 @@ int  check_prime(int n, int resp);
 
 int is_palindrome(char *s);
 
+int check(char *s, int start, int end, int pair);
+
+int last_index(char *s);
+
 int wildcmp(char *s1, char *s2);
 
 int _putchar(char c);
