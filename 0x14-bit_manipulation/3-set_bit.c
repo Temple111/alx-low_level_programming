34fd7e89b5d55848a4a17c1114ec7f7ab1c9592f
Temple111 <templeeseigbe014@gmail.com>
2023-04-07 11:22:03 -0700

    g

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,11 +8,10 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	 *n ^= (1 << index);
 
 	if (index >= (sizeof(unsigned long int)* 8))
 		return (-1);
-
-	*n ^= (1 << index);
 
 	return (1);
 }
