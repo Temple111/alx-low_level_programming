f433535d11ed6f1c1a3a2989484de62b0c2270eb
Temple111 <templeeseigbe014@gmail.com>
2023-04-09 13:39:20 -0700

    j

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,23 +9,23 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int op, wr, i;
+	int o, w, len = 0;
 
-	i = 0;
 	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
 	{
-		for (; text_content[i];)
-			i++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0000);
-	wr = write(op, text_content, i);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
-	if (op == -1 || wr == -1)
+	if (o == -1 || w == -1)
 		return (-1);
 
-	close(op);
+	close(o);
+
 	return (1);
 }
