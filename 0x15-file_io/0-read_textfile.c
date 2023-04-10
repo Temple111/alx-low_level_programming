de4bf3c7894b12a85b347a4cfd39ce32de60aa88
Temple111 <templeeseigbe014@gmail.com>
2023-04-10 07:37:56 -0700

    file i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,28 +10,28 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t nrd, nwr;
+	int op;
+	ssize_t nd, wr;
 	char *buf;
 
 	if (!filename)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
+	op = open(filename, O_RDONLY);
 
-	if (fd == -1)
+	if (op == -1)
 		return (0);
 
 	buf = malloc(sizeof(char) * (letters));
 	if (!buf)
 		return (0);
 
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
+	nd = read(op, buf, letters);
+	wr = write(STDOUT_FILENO, buf, nd);
 
-	close(fd);
+	close(op);
 
 	free(buf);
 
-	return (nwr);
+	return (wr);
 }
